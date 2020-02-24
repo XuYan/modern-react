6f9c32d0bdfe31551c92f58333f4dbb15ecc1b85
XuYan <xuwithwill@gmail.com>
2020-02-24 11:53:08 -0800

    complete exercise 4

--- src/__tests__/04.js
+++ src/__tests__/04.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {render, fireEvent, flushEffects} from 'react-testing-library'
-import Usage from '../exercises-final/04'
-// import Usage from '../exercises/04'
+import { render, fireEvent, flushEffects } from 'react-testing-library'
+// import Usage from '../exercises-final/04'
+import Usage from '../exercises/04'
 
 afterEach(() => {
   window.localStorage.removeItem('count')
@@ -9,7 +9,7 @@ afterEach(() => {
 
 test('Usage works', () => {
   window.localStorage.setItem('count', 3)
-  const {container} = render(<Usage />)
+  const { container } = render(<Usage />)
   const button = container.getElementsByTagName('button')[0]
   expect(button).toHaveTextContent(/3/)
   fireEvent.click(button)
@@ -33,4 +33,22 @@ test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
   expect(submitted).toBe(true)
 })
+/**
+ * 1. Lazy Initial State:
+ * If the initial state is the result of an expensive computation,
+ * you may provide a function instead,
+ * which will be executed only on the initial render.
+ *
+ * Lazy Initial State: useState(() => {})
+ *
+ * 2. useEffects(() => {}, [dependencies])
+ * React _only_ calls the effect callback:
+ * i. After the first render
+ * ii. After a render during which any element in the dependencies array changes.
+ * (If there is no array provided, then it is called after every render.)
+ *
+ * 3. If there are multiple things you need to do, like subscribe some stuff,
+ *    send http request, access local storage, etc. Each of these things should
+ *    be in their own useEffect callback
+ */
 ////////////////////////////////
--- src/exercises/04.js
+++ src/exercises/04.js
@@ -1,19 +1,17 @@
 // Counter: optimizations
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Counter() {
   // Right now, we're reading localStorage on every render
   // But we only really need to read that value for the first render
   // 🐨 1. instead of passing the value to useState as we are now,
   // pass a function which returns the value.
-  const [count, setCount] = useState(
-    Number(window.localStorage.getItem('count') || 0),
-  )
+  const [count, setCount] = useState(() => Number(window.localStorage.getItem('count') || 0))
   const incrementCount = () => setCount(count + 1)
   useEffect(
     () => {
       window.localStorage.setItem('count', count)
-    },
+    }, [count]
     // 🐨 2. pass a second argument to useEffect right here
     // it should be an array of the callback's "dependencies"
     // Said differently: "What variables does the useEffect callback use?"
