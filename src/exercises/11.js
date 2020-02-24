47906eefb589faf87c1dbd5e768c6a2d250689c5
XuYan <xuwithwill@gmail.com>
2020-02-24 12:00:36 -0800

    complete 11

--- src/exercises/11.js
+++ src/exercises/11.js
@@ -10,3 +10,15 @@ function Usage() {
 Usage.title = 'Where to NOT use hooks'
 
 export default Usage
+
+/**
+ * React Hooks can only be used in function component. You cannot use them in class component.
+ * The only place hooks are allowed to be used is in the rendering phase of a function component. DO NOT use hooks in callbacks, etc.
+ *
+ * DO NOT use hooks conditionally
+ * because the order of hooks matter, when Component gets re-rendered, React does not know what’s the right value to return.
+ *
+ * DO NOT use hooks in loops.
+ *
+ * Cannot call hooks outside a function body of a function component.
+ */
