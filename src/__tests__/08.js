ba2d280544bd91e6b9f3e875c85576d967fc3f2a
XuYan <xuwithwill@gmail.com>
2020-02-24 11:57:58 -0800

    complete exercise 8

--- src/__tests__/08.js
+++ src/__tests__/08.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import chalk from 'chalk'
-import {render, fireEvent} from 'react-testing-library'
-import Usage from '../exercises-final/08'
-// import Usage from '../exercises/08'
+import { render, fireEvent } from 'react-testing-library'
+// import Usage from '../exercises-final/08'
+import Usage from '../exercises/08'
 
 const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
 test('renders', async () => {
   jest.spyOn(console, 'error')
-  const {container, unmount, getByText} = render(<Usage />)
+  const { container, unmount, getByText } = render(<Usage />)
   fireEvent.click(getByText('Start'))
   const label = container.querySelector('label')
   await sleep(20)
@@ -63,4 +63,7 @@ test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
   expect(submitted).toBe(true)
 })
+/**
+ * alias useReducer dispatch to setState.
+ */
 ////////////////////////////////
